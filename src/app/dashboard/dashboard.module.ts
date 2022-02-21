83b664085a4cfb01c4a597dc261d85fad82ba2a2
jonvalenz <jon.valenzuela@mllrdev.com>
2022-02-21 12:09:13 +0800

    Reorganize dashboard imports

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,16 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
-import { MatInputModule } from '@angular/material/input';
-import { CategoryComponent } from './category/category.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+
+import { CategoryComponent } from './category/category.component';
+import { DashboardComponent } from './dashboard.component';
 import { ListComponent } from './list/list.component';
 import { TaskComponent } from './task/task.component';
 
